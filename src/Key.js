760dedb8a2a5a45c486db899c5e75b30e631a6d5
devbeau <48367657+devbeau@users.noreply.github.com>
2021-04-05 14:21:29 -0400

    basic click and release functionality

--- src/Key.js
+++ src/Key.js
@@ -1,25 +1,28 @@
 function Key (props) {
     let context = new AudioContext();
     let note = context.createOscillator();
-    const real = new Float32Array(2);
-    const imag = new Float32Array(2);
-    real[0] = 0;
-    imag[0] = 0;
-    real[1] = 1;
-    imag[0] = 0;
-
-    var wave = context.createPeriodicWave(real, imag, {disableNormalization: true});
-    note.frequency.value = 440;
-    note.setPeriodicWave(wave);
     note.type = "square";
-    note.connect(context.destination);
+    note.frequency.value = props.noteInfo[1];
+    note.connect(context.destination)
 
-    note.start();
-    note.stop(1);
-    note.frequency.value *= 1.0595
-    console.log(props)
+    function onMouseDown() {
+        note.start();
+    }
+    function onMouseUp() {
+        note.stop();
+        note = context.createOscillator();
+        note.type = "triangle";
+        note.frequency.value = props.noteInfo[1];
+        note.connect(context.destination)
+    }
     return (
-        <div className="key">{props.noteInfo[0]}</div>
+        <div 
+        className="key"
+        onMouseDown={e => {onMouseDown()}}
+        onMouseUp= {e => {onMouseUp()}}
+        >
+            {props.noteInfo[0]}
+        </div>
     )
 }
 
